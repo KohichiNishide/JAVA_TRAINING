052cd7bb2f0ec480e8b1019920437510e11d29c7
Your Name <kohichi.nishide@gmail.com>
2012-04-22 13:55:00 +0900

    test commit

--- JPL/ch03/ex01/PassengerVehicle.java
+++ JPL/ch03/ex01/PassengerVehicle.java
@@ -1,4 +1,52 @@
+package ex01;
 
-public class PassengerVehicle {
-
+public class PassengerVehicle extends Vehicle{
+	private int nSeats; //Ô‚ª‚Á‚Ä‚¢‚éÀÈ”
+	private int nSeatedPeople; //Œ»İÀ‚Á‚Ä‚¢‚él”
+	
+	public PassengerVehicle(String owner, int nSeats) {
+		super(owner);
+		this.nSeats = nSeats;
+	}
+	
+	public String toString() {
+		String str = super.toString() + " Number of Seats : " + nSeats + " Number of Seated people : " + nSeatedPeople;
+		return str;
+	}
+	
+	//------------------------------------
+	// GetŠÖ”
+	//------------------------------------
+	public int getNoOfSeats() {
+		return nSeats;
+	}
+	
+	public int getNoOfSeatedPeople() {
+		return nSeatedPeople;
+	}
+	
+	//------------------------------------
+	// SetŠÖ”
+	//------------------------------------	
+	public void setNoOfSeatedPeople(int nSeatedPeople) {
+		this.nSeatedPeople = nSeatedPeople;
+	}
+	
+	/*
+	 * MainŠÖ”
+	 */
+	static void main(String[] args) {
+		PassengerVehicle smallCar = new PassengerVehicle(args[0], 2);
+		PassengerVehicle normalCar = new PassengerVehicle(args[1], 5);
+		PassengerVehicle bigCar = new PassengerVehicle(args[2], 10);
+		
+		smallCar.setNoOfSeatedPeople(1);
+		normalCar.setNoOfSeatedPeople(1);
+		bigCar.setNoOfSeatedPeople(1);
+		
+		System.out.println(smallCar.toString());
+		System.out.println(normalCar.toString());
+		System.out.println(bigCar.toString());
+		System.out.println("maxID : " + Vehicle.getMaxID());
+	}
 }
---
+++ JPL/ch03/ex01/PassengerVehicleTest.java
@@ -0,0 +1,55 @@
+package ex01;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.BufferedOutputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class PassengerVehicleTest {
+	private PrintStream saved;
+	private final ByteArrayOutputStream out = new ByteArrayOutputStream();
+	
+	/**
+	 * System.out.printlnの出力先をByteArrayOutputStreamに変更する
+	 */
+	@Before 
+	public void before() {
+		saved = System.out;
+		System.setOut(new PrintStream(new BufferedOutputStream(out)));
+	}
+	
+	/**
+	 * System.out.printlnの出力先をコンソールに戻す
+	 */
+	@After
+	public void after() {
+		System.setOut(saved);
+	}
+	
+	@Test
+	public void mainTest() {
+		String[] owners = {"Tom", "Kelly", "John"};
+		PassengerVehicle.main(owners);
+		System.out.flush(); //結果の出力を保証するために強制出力
+		String expected = joinStrings("0 CurrentSpeed : 100.0 CurrentAngle : 10.0 Owner : Tom Number of Seats : 2 Number of Seated people : 1"
+				, "1 CurrentSpeed : 100.0 CurrentAngle : 10.0 Owner : Kelly Number of Seats : 5 Number of Seated people : 1"
+				, "2 CurrentSpeed : 100.0 CurrentAngle : 10.0 Owner : John Number of Seats : 10 Number of Seated people : 1"
+				, "maxID : 2");
+		String actual = out.toString();	
+		assertEquals(expected, actual);
+	}
+
+	private String joinStrings(String... strs) {
+        String newLine = System.getProperty("line.separator");//環境依存である改行コードの読み込み
+        String result = "";
+        for (String s : strs) {
+            result += s + newLine;
+        }
+        return result;
+    }
+}
