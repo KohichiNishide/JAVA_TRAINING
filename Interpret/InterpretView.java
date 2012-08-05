85379c46ebc8274eda62f550630f9878ed71f1d5
Your Name <kohichi.nishide@gmail.com>
2012-08-05 15:43:39 +0900

    Improve interpret

--- Interpret/InterpretView.java
+++ Interpret/InterpretView.java
@@ -1,64 +1,105 @@
+import java.awt.BorderLayout;
 import java.awt.Button;
+import java.awt.Choice;
 import java.awt.Font;
 import java.awt.Frame;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
 import java.awt.Label;
+import java.awt.Scrollbar;
 import java.awt.TextField;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
+import java.awt.event.AdjustmentEvent;
+import java.awt.event.AdjustmentListener;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Observable;
 import java.util.Observer;
 
 
-public class InterpretView extends Frame implements Observer, ActionListener{
+public class InterpretView extends Frame implements Observer, ActionListener, AdjustmentListener{
 	private static final long serialVersionUID = 1L;
+	private Scrollbar bar;
 	private Label objectNameLabel = new Label("Input class name");
 	private Label successLabel = new Label("TBD");
-	private TextField objectNameTextField = new TextField("Input class name");
+	private Label fieldNameLabel = new Label("Name");
+	private Label fieldTypeLabel = new Label("Type");
+	private Label fieldValLabel = new Label("Value");
+	private Choice fieldsChoice = new Choice();
+	private TextField objectNameTextField = new TextField("java.awt.Frame");
 	private Button okButton = new Button("Create object");
 	private GridBagLayout gbl = new GridBagLayout();
 	private Interpret con;
+	static final int COMPONENT_COUNT = 45;
+	private Label[] fieldNames = new Label[COMPONENT_COUNT];
+	private Label[] fieldTypes = new Label[COMPONENT_COUNT];
+	private TextField[] fieldValues = new TextField[COMPONENT_COUNT];
 	
 	public InterpretView(Interpret controller) {
 		con = controller;
 		setTitle("Interpret main view");
-	    setSize(600, 200);
+	    setSize(1200, 1200);
 	    setLocationRelativeTo(null);
 	    setResizable(false);
 	    setLayout(gbl);
 	    
+	   
+        bar = new Scrollbar(Scrollbar.VERTICAL, 0, 10, 0, 1000);
+	    
+        bar.addAdjustmentListener(this);
 	    okButton.addActionListener(this);
 	    okButton.addActionListener(con);
 	    
 	    addComponents();
 	        
         addWindowListener(con);       
-        pack();
+        //pack();
         show();
+	}
+	
+	private void addBar(Scrollbar bar) {
+		GridBagConstraints gbc = new GridBagConstraints();
+        gbc.fill = GridBagConstraints.BOTH;
+        gbc.weightx = 100.0;
+        gbc.weighty = 100.0;
+        gbc.anchor = GridBagConstraints.EAST;
+        gbc.insets = new Insets(10, 10, 10, 10);
+        gbl.setConstraints(bar, gbc);
+        add(bar, BorderLayout.EAST);
 	}
 	
 	private void addComponents() {
 		// ラベルの配置
         //objectNameLabel.setFont(new Font("Arial", Font.PLAIN, 20));      
         //addLabel(objectNameLabel, 0, 0, 1, 1);
+
+        addLabel(fieldNameLabel, 0, 10, 3, 1);
+        addLabel(fieldTypeLabel, 3, 10, 3, 1);
+        addLabel(fieldValLabel, 6, 10, 4, 1);
         
-        successLabel.setFont(new Font("Arial", Font.PLAIN, 20));      
-        addLabel(successLabel, 0, 3, 3, 1);
+        //successLabel.setFont(new Font("Arial", Font.PLAIN, 20));      
+        //addLabel(successLabel, 0, 3, 3, 1);
         
         // テキストフィールドの配置
-        addTextField(objectNameTextField, 0, 1, 3, 1);
+        addTextField(objectNameTextField, 0, 0, 10, 1);     
         
+        for (int i = 0; i < COMPONENT_COUNT; i++) {
+        	fieldNames[i] = new Label();
+        	fieldTypes[i] = new Label();
+        	fieldValues[i] = new TextField();
+        	addLabel(fieldNames[i], 0, 11 + i, 3, 1);
+        	addLabel(fieldTypes[i], 3, 11 + i, 3, 1);
+        	addTextField(fieldValues[i], 6, 11 + i, 4, 1);
+        }
         // ボタンフィールドの配置
-        addButton(okButton, 2, 2, 1, 1);
+        addButton(okButton, 10, 0, 1, 1);
 	}
 	
 	private void addButton(Button button, int x, int y, int w, int h) {
 		GridBagConstraints gbc = new GridBagConstraints();
-        gbc.fill = GridBagConstraints.NONE;
+        gbc.fill = GridBagConstraints.BOTH;
         gbc.weightx = 100.0;
         gbc.weighty = 100.0;
         gbc.gridx = x;
@@ -88,7 +129,7 @@ public class InterpretView extends Frame implements Observer, ActionListener{
 	
 	private void addTextField(TextField tf, int x, int y, int w, int h) {
         GridBagConstraints gbc = new GridBagConstraints();
-        gbc.fill = GridBagConstraints.BOTH;
+        gbc.fill = GridBagConstraints.HORIZONTAL;
         gbc.weightx = 100.0;
         gbc.weighty = 100.0;
         gbc.gridx = x;
@@ -100,19 +141,52 @@ public class InterpretView extends Frame implements Observer, ActionListener{
         gbl.setConstraints(tf, gbc);
         add(tf);
     }
+	
+	private void addChoice(Choice choice, int x, int y, int w, int h) {
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.fill = GridBagConstraints.BOTH;
+        gbc.weightx = 100.0;
+        gbc.weighty = 100.0;
+        gbc.gridx = x;
+        gbc.gridy = y;
+        gbc.gridwidth = w;
+        gbc.gridheight = h;
+        gbc.anchor = GridBagConstraints.WEST;
+        gbc.insets = new Insets(5, 5, 5, 5);
+        gbl.setConstraints(choice, gbc);
+        add(choice);
+    }
 
 	@Override
 	public void update(Observable obs, Object obj) {
-		if (!((ObjectInfo)obs).IsError()) {
-			successLabel.setText("Success !!");
+		ObjectInfo model = (ObjectInfo)obs;
+		if (!model.IsError()) {
+			successLabel.setText("Success !!");			
+			List<String> fieldNameList = model.getFieldNames();
+			for (int i = 0; i < fieldNameList.size(); i++) {
+				fieldNames[i].setText(fieldNameList.get(i));
+				fieldTypes[i].setText(model.getFieldTypes().get(i));
+				fieldValues[i].setText(model.getFieldVals().get(i));
+			}
+			
+			//ObjectPropertyDialog dlg = new ObjectPropertyDialog(this, con);
+			//dlg.show();
 		} else {
 			successLabel.setText("Failed !!");
+			for (int i = 0; i < fieldNames.length; i++) {
+				fieldNames[i].setText("");
+			}
 		}
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		con.setObjectName(objectNameTextField.getText());
+	}
+
+	@Override
+	public void adjustmentValueChanged(AdjustmentEvent arg0) {
+		// TODO Auto-generated method stub
 		
 	}
 }
--- Interpret/ObjectInfo.java
+++ Interpret/ObjectInfo.java
@@ -1,4 +1,5 @@
 import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Observable;
@@ -7,12 +8,23 @@ import java.util.Observable;
 public class ObjectInfo extends Observable{
 	public Object obj;
 	public List<Field> fields = new ArrayList<Field>();
+	public List<Method> methods = new ArrayList<Method>();
 	public List<String> fieldNames = new ArrayList<String>();
+	public List<String> fieldTypes = new ArrayList<String>();
+	public List<String> methodNames = new ArrayList<String>();
+	public List<String> methodTypes = new ArrayList<String>();
+	public List<String> fieldVals = new ArrayList<String>();
 	Boolean isError = false;
 	
 	public void saveObject(Object o) {
 		obj = o;
+		reset();
 		saveFields(getAllFieldsFromClass(o.getClass()));
+		saveMethods(getAllMethodsFromClass(o.getClass()));
+
+		isError = false;
+		setChanged();
+		notifyObservers();
 	}
 	
 	public Boolean IsError() {
@@ -27,13 +39,41 @@ public class ObjectInfo extends Observable{
 	
 	private void saveFields(List<Field> fs) {
 		fields = fs;
-		fieldNames.clear();
+		
 		for (Field f : fields) {
+			f.setAccessible(true);
 			fieldNames.add(f.getName());
-		}
-		isError = false;
-		setChanged();
-		notifyObservers();
+			fieldTypes.add(f.getType().toString());
+			try {
+				fieldVals.add(f.get(obj).toString());
+			} catch (NullPointerException e) {				
+				e.printStackTrace();
+				fieldVals.add("Null");
+			} catch (IllegalArgumentException e) {
+				e.printStackTrace();
+				fieldVals.add("Impossible");
+			} catch (IllegalAccessException e) {
+				e.printStackTrace();
+				fieldVals.add("Impossible");
+			}
+		}		
+	}
+	
+	private void saveMethods(List<Method> ms) {
+		methods = ms;
+		
+		for (Method m : methods) {
+			m.setAccessible(true);
+			methodNames.add(m.getName());
+			methodTypes.add(m.getGenericReturnType().toString());
+		}		
+	}
+	
+	private void reset() {
+		fieldNames.clear();
+		fieldTypes.clear();
+		methodNames.clear();
+		methodTypes.clear();
 	}
 	
 	private List<Field> getAllFieldsFromClass(Class cls) {
@@ -54,7 +94,33 @@ public class ObjectInfo extends Observable{
 		return allFields;
 	}
 	
+	private List<Method> getAllMethodsFromClass(Class cls) {
+		List<Method> allMethods = new ArrayList<Method>(); 
+		Method[] methods = cls.getDeclaredMethods();
+		Method[] publicMethods = cls.getMethods();
+		for (Method m : publicMethods) {
+            if(m.getDeclaringClass() == Object.class)
+                    continue;
+            if(cls.toString().equals(m.getDeclaringClass().toString()))
+                    continue;
+            allMethods.add(m);
+        }
+		
+		for (Method m : methods) {
+			allMethods.add(m);
+		}
+		return allMethods;
+	}
+	
 	public final List<String> getFieldNames(){
 	    return fieldNames;
+	}
+	
+	public final List<String> getFieldTypes(){
+	    return fieldTypes;
+	}
+	
+	public final List<String> getFieldVals(){
+	    return fieldVals;
 	}
 }
