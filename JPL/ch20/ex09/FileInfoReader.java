f6ea9d90b2dd406ef3c4c07d3b6eea52ce047ceb
Your Name <kohichi.nishide@gmail.com>
2012-09-12 02:19:02 +0900

    Fix ch20.ex09

--- JPL/ch20/ex09/FileInfoReader.java
+++ JPL/ch20/ex09/FileInfoReader.java
@@ -4,29 +4,37 @@ import java.io.File;
 import java.io.IOException;
 
 public class FileInfoReader {
-	public static void showFileProperty(File file) throws IOException {
-		System.out.println("getName() = " + file.getName());
-		System.out.println("getPath() = " + file.getPath());
-		System.out.println("getAbsolutePath() = " + file.getAbsolutePath());
-		System.out.println("getCanonicalPath() = " + file.getCanonicalPath());
-		System.out.println("getParent() = " + file.getParent());
-		System.out.println("lastModified() = " + file.lastModified());
-		System.out.println("length() = " + file.length());
-		System.out.print("list() = ");
-		String[] files = file.list();
-		if (files != null) {
-			for (String f : files) {
-				System.out.print(f + ", ");
+	public static void showFileProperty(File[] files) throws IOException {
+		if (files == null)
+			return;
+		
+		for (File file : files) {
+			System.out.println("getName() = " + file.getName());
+			System.out.println("getPath() = " + file.getPath());
+			System.out.println("getAbsolutePath() = " + file.getAbsolutePath());
+			System.out.println("getCanonicalPath() = " + file.getCanonicalPath());
+			System.out.println("getParent() = " + file.getParent());
+			System.out.println("lastModified() = " + file.lastModified());
+			System.out.println("length() = " + file.length());
+			System.out.print("list() = ");
+			String[] sameDirFiles = file.list();
+			if (sameDirFiles != null) {
+				for (String f : sameDirFiles) {
+					System.out.print(f + ", ");
+				}
+			} else {
+				System.out.print("0");
 			}
-		} else {
-			System.out.print("0");
+			System.out.println("");
 		}
-		System.out.println("");
 	}
 	
 	public static void main(String[] args) throws IOException {
-		final String INPUT_FILE_PATH = "JPL/ch20/ex09/input.txt";
-		File file = new File(INPUT_FILE_PATH);
-		showFileProperty(file);
+		final String INPUT_FILE_PATH_1 = "JPL/ch20/ex09/input1.txt";
+		final String INPUT_FILE_PATH_2 = "JPL/ch20/ex09/input2.txt";
+		File file1 = new File(INPUT_FILE_PATH_1);
+		File file2 = new File(INPUT_FILE_PATH_2);
+		File[] files = {file1, file2};
+		showFileProperty(files);
 	}
 }
