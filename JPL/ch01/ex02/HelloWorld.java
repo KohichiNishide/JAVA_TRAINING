3f7402fa25ee66d679576cc93f378768ac018c2e
kNishide <kohichi.nishide@nts.ricoh.co.jp>
2012-12-01 15:58:56 +0900

    Fix complie bugs

--- JPL/ch01/ex02/HelloWorld.java
+++ JPL/ch01/ex02/HelloWorld.java
@@ -4,7 +4,7 @@ package ch01.ex02;
  * ��K���1.2�FHelloWorld�̈ꕔ��ύX���āA�ǂ̂悤�ȃG���[���������邩���ׂ�B
  */
 
-private class HelloWorld {
+public class HelloWorld {
 	/** HelloWorld�N���X��private�錾�ɂ���ƈȉ��̃G���[�������B
 	 * "Illegal modifier for the class HelloWorld; only public, abstract & final are permitted"
 	 * 
