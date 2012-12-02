ac83ca3761a099c54f9ea3caf198340c42edf78f
kNishide <kohichi.nishide@nts.ricoh.co.jp>
2012-12-02 23:12:44 +0900

    Fix ch21.ex05

--- JPL/ch21/ex05/ArrayBunchList.java
+++ JPL/ch21/ex05/ArrayBunchList.java
@@ -46,15 +46,20 @@ public class ArrayBunchList<E> extends AbstractList<E> {
 		return size;
 	}
 	
+	@Override
+	public ListIterator<E> listIterator() {
+		return new ABLListIterator();
+	}
+	
 	//********************
-	//  Iterator
+	//  ListIterator
 	//********************
-	private class ABLIterator implements Iterator<E>{
+	public class ABLListIterator implements ListIterator<E> {
 		private int off; // リストの先頭からのオフセット
 		private int array; // 現在処理している配列
 		private int pos; // 現在の配列内の位置
-
-		public ABLIterator() {
+		boolean isForbiddenSet = false;
+		public ABLListIterator() { 
 			off = 0;
 			array = 0;
 			pos = 0;
@@ -65,11 +70,69 @@ public class ArrayBunchList<E> extends AbstractList<E> {
 		}
 		
 		@Override
+		//指定された要素をリストに挿入します (任意のオペレーション)。
+		//要素は、next によって返される次の要素があればその要素の直前、および previous によって返される次の要素があればその要素の直後に挿入されます。
+		//リストに要素がない場合は、新しい要素がリストの唯一の要素になります。新しい要素は、暗黙のカーソルの前に挿入されます。
+		//後続の next の呼び出しは影響を受けず、後続の previous の呼び出しは新しい要素を返します。
+		//この呼び出しは、nextIndex または previousIndex の呼び出しによって返される値を 1 増やします。 
+		public void add(E e) {
+			//TBD
+			isForbiddenSet = false;
+			throw new UnsupportedOperationException();
+		}
+
+		@Override
+		//リストを逆方向にトラバースしたときに、リスト反復子がさらに要素を持っている場合に true を返します。
+		//つまり、previous の呼び出しが例外をスローすることなく要素を返す場合は、true を返します。 
+		public boolean hasPrevious() {
+			return off + pos > 0;
+		}
+
+		@Override
+		//リストの前の要素を返します。
+		//このメソッドは、リストを逆方向に反復するために繰り返し呼び出される場合と、前後に移動するために next の呼び出しと組み合わされる場合があります。
+		//next と previous の呼び出しを交互に行うと、繰り返し同じ要素が返されます。 
+		public E previous() {
+			if (!hasPrevious())
+				throw new NoSuchElementException();
+			E ret = arrays[array][pos--];
+			while(pos < 0){
+				off -= arrays[array--].length;
+				pos = arrays[array].length - 1;
+			}
+			isForbiddenSet = true;
+			return ret;
+		}
+
+		@Override
+		// 次に previous を呼び出したときに返されることになる要素のインデックスを返します。リスト反復子がリストの先頭にある場合は -1 を返します。 
+		public int previousIndex() {
+			if (hasPrevious())
+				return off + pos -1;
+			else
+				return -1;
+		}
+
+		@Override
+		//next または previous から最後に返された要素を指定された要素で置き換えます (任意のオペレーション)。
+		//この呼び出しは、前回の next または previous の呼び出し以降に ListIterator.remove と ListIterator.add のどちらも呼び出されていない場合にだけ行うことができます。 
+		public void set(E e) {
+			if (isForbiddenSet)
+				throw new IllegalStateException();
+			arrays[array][pos] = e;
+		}
+
+		@Override
+		//リストを順方向にトラバースしたときに、リスト反復子がさらに要素を持っている場合に true を返します。
+		//つまり、next の呼び出しが例外をスローすることなく要素を返す場合は、true を返します。 
 		public boolean hasNext() {
 			return off + pos < size();
 		}
 
 		@Override
+		//リスト内の次の要素を返します。
+		//このメソッドは、リストを反復するために繰り返し呼び出される場合と、前後に移動するために previous の呼び出しと組み合わされる場合があります。
+		//next と previous の呼び出しを交互に行うと、繰り返し同じ要素が返されます。
 		public E next() {
 			if (!hasNext())
 				throw new NoSuchElementException();
@@ -81,57 +144,27 @@ public class ArrayBunchList<E> extends AbstractList<E> {
 				if (array >= arrays.length)
 					break;
 			}
+			isForbiddenSet = true;
 			return ret;
 		}
-
-		@Override
-		public void remove() {
-			throw new UnsupportedOperationException();
-		}
-	}
-	//********************
-	//  ListIterator
-	//********************
-	public class ABLListIterator extends ABLIterator implements ListIterator<E> {
-		
-		public ABLListIterator() { 
-			super();
-		}
 		
 		@Override
-		public void add(E e) {
-			int addIndex = this.nextIndex();
-		    // TBD
-		}
-
-		@Override
-		public boolean hasPrevious() {
-			// TODO Auto-generated method stub
-			return false;
-		}
-
-		@Override
+		//次に next を呼び出したときに返されることになる要素のインデックスを返します。リスト反復子がリストの末尾にある場合はリストのサイズを返します。 
 		public int nextIndex() {
-			// TODO Auto-generated method stub
-			return 0;
+			if (hasNext())
+				return off + pos + 1;
+			else
+				return size();
 		}
 
 		@Override
-		public E previous() {
-			// TODO Auto-generated method stub
-			return null;
-		}
-
-		@Override
-		public int previousIndex() {
-			// TODO Auto-generated method stub
-			return 0;
-		}
-
-		@Override
-		public void set(E e) {
-			// TODO Auto-generated method stub
-			
+		//next または previous によって返された最後の要素をリストから削除します (任意のオペレーション)。
+		//この呼び出しは next または previous の呼び出しごとに 1 回だけ行うことができます。
+		//この呼び出しは、前回の next または previous の呼び出し以降に ListIterator.add が呼び出されていない場合にだけ行うことができます。 
+		public void remove() {
+			// TBD
+			isForbiddenSet = false;
+			throw new UnsupportedOperationException();
 		}  
 	}
 }
